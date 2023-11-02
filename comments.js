475c510100c2d9c30efc2f5610f53e2fde90c6c1
strikerX121 <58236369+strikerX121@users.noreply.github.com>
2023-11-02 13:58:45 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,15 +1,29 @@
-//Create Web Server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+// Create a webserver 
+// 1. create a webserver
+// 2. create a route for the webserver
+// 3. create a request handler for the route
+// 4. create a response for the request handler
+// 5. start the server
+// 6. open the browser and type localhost:3000
+// 7. stop the server by pressing ctrl + c
+
+var http = require('http');
 var fs = require('fs');
-var path = require('path');
 
-//Create Web Server
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+// create a server
+var server = http.createServer();
 
-//Create Web Server
-app.listen(80, function() {
-    console.log('Server running at http://
+// create a route
+server.on('request', function(req, res) {
+    console.log('request received');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./index.html', function(err, data) {
+        if (err) throw err;
+        res.end(data);
+    });
+});
+
+// start the server
+server.listen(3000, function() {
+    console.log('server started at port 3000');
+});
