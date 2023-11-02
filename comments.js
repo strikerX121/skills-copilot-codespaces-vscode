2977e5eadcddcdec7b78989dc9f12d3a2b1f1c6b
strikerX121 <58236369+strikerX121@users.noreply.github.com>
2023-11-02 14:01:59 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,8 +1,76 @@
-// Create a webserver 
-// 1. create a webserver
-// 2. create a route for the webserver
-// 3. create a request handler for the route
-// 4. create a response for the request handler
-// 5. start the server
-// 6. open the browser and type localhost:3000
-// 7. stop the server by pressing ctrl + c
+// Create a webserver
+// Listen for requests
+// Parse the request
+// Handle the request
+// Respond to the request
+// Close the request
+
+// Import the http module
+const http = require('http');
+// Import the fs module
+const fs = require('fs');
+// Import the url module
+const url = require('url');
+
+// Import the comments module
+const comments = require('./comments.js');
+
+// Create a web server
+const server = http.createServer(function (request, response) {
+  // Log the request
+  console.log(`New request: ${request.url}`);
+
+  // Parse the request
+  const parsedUrl = url.parse(request.url, true);
+  const path = parsedUrl.pathname;
+  const query = parsedUrl.query;
+
+  // Handle the request
+  if (path === '/') {
+    // Respond to the request
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('<h1>Hello World!</h1>');
+    response.end();
+  } else if (path === '/comments') {
+    // Respond to the request
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.write(JSON.stringify(comments));
+    response.end();
+  } else if (path === '/comments/add') {
+    // Parse the body
+    let body = '';
+    request.on('data', function (chunk) {
+      body += chunk;
+    });
+    request.on('end', function () {
+      // Add the comment
+      comments.push(body);
+      // Respond to the request
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify(comments));
+      response.end();
+    });
+  } else if (path === '/comments/delete') {
+    // Parse the body
+    let body = '';
+    request.on('data', function (chunk) {
+      body += chunk;
+    });
+    request.on('end', function () {
+      // Delete the comment
+      comments.splice(body, 1);
+      // Respond to the request
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      response.write(JSON.stringify(comments));
+      response.end();
+    });
+  } else {
+    // Respond to the request
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.write('<h1>Not Found</h1>');
+    response.end();
+  }
+});
+
+// Listen for requests
+server.listen
