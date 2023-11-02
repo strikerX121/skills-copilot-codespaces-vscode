039181b167edcb641d68b3d1465da607fd395f06
strikerX121 <58236369+strikerX121@users.noreply.github.com>
2023-11-02 13:59:56 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -6,24 +6,3 @@
 // 5. start the server
 // 6. open the browser and type localhost:3000
 // 7. stop the server by pressing ctrl + c
-
-var http = require('http');
-var fs = require('fs');
-
-// create a server
-var server = http.createServer();
-
-// create a route
-server.on('request', function(req, res) {
-    console.log('request received');
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile('./index.html', function(err, data) {
-        if (err) throw err;
-        res.end(data);
-    });
-});
-
-// start the server
-server.listen(3000, function() {
-    console.log('server started at port 3000');
-});
